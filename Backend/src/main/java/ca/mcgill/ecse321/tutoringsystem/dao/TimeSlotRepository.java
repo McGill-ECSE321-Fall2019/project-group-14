94c550974b1c4cc7d3172f32e418abe6548bd2c4
Nelson Zeng <nelson.zeng25@gmail.com>
2019-10-11 16:11:32 -0400

    Fixed typos

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dao/TimeSlotRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dao/TimeSlotRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.tutoringsystem.model.TimeSlot;
 
 public interface TimeSlotRepository extends CrudRepository<TimeSlot, Integer>{
-  TimeSlot findTimeSlotbyId(int timeSlotId);
+  TimeSlot findTimeSlotById(int timeSlotId);
   List<TimeSlot> findTimeSlotByDateAndTime(Date date, Time time);
 }
