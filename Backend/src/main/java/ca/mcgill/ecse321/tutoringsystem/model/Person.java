c4685bf5b4ddb8765e6eb6e607f476449a0d386d
Mehdi <kabylfx@gmail.com>
2019-10-12 13:56:47 -0400

    replace person.java and uml files

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Person.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Person.java
@@ -1,45 +1,42 @@
 package ca.mcgill.ecse321.tutoringsystem.model;
 
-import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.GeneratedValue;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@DiscriminatorColumn(name = "person_type")
-public class Person {
-  private String name;
+public class Person{
+   private String name;
 
-  public void setName(String value) {
+public void setName(String value) {
     this.name = value;
-  }
-
-  public String getName() {
+}
+public String getName() {
     return this.name;
-  }
+}
+private String email;
 
-  private String email;
-
-  public void setEmail(String value) {
+public void setEmail(String value) {
     this.email = value;
-  }
-
-  public String getEmail() {
+}
+public String getEmail() {
     return this.email;
-  }
+}
+private Integer userId;
 
-  private Integer userId;
-
-  public void setUserId(Integer value) {
+public void setUserId(Integer value) {
     this.userId = value;
-  }
-
-  @Id
-  @GeneratedValue()
-  public Integer getUserId() {
+}
+@Id
+@GeneratedValue()public Integer getUserId() {
     return this.userId;
-  }
+}
+private String password;
+
+public void setPassword(String value) {
+    this.password = value;
+}
+public String getPassword() {
+    return this.password;
+}
 }
