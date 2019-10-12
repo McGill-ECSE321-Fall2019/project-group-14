2ea1cda16250178c38384b4bd65037e7709bab0d
Tian Ci Liu <the.liutianci@gmail.com>
2019-10-11 22:12:57 -0400

    Add user_type discriminator to Person

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Manager.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Manager.java
@@ -1,10 +1,12 @@
 package ca.mcgill.ecse321.tutoringsystem.model;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import java.util.Set;
 import javax.persistence.OneToMany;
 
 @Entity
+@DiscriminatorValue("MANAGER")
 public class Manager extends Person{
    private Set<Application> application;
    
