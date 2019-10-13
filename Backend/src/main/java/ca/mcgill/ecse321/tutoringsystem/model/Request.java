54ab3f0962046ad08c1fce6051f4f352c9cda8c5
Tian Ci Liu <the.liutianci@gmail.com>
2019-10-12 20:09:32 -0400

    all create and read tests working... had to remove Session class. See closed issue for more details

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Request.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/Request.java
@@ -4,96 +4,100 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import java.sql.Time;
+import java.sql.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.DynamicUpdate;
 
-import java.sql.Time;
-import java.sql.Date;
-
 @Entity
-@DynamicUpdate
-public class Request{
-   private Tutor tutor;
-   
-   @ManyToOne(optional=false)
-   public Tutor getTutor() {
-      return this.tutor;
-   }
-   
-   public void setTutor(Tutor tutor) {
-      this.tutor = tutor;
-   }
-   
-   private Integer requestId;
+public class Request {
+	private Tutor tutor;
 
-public void setRequestId(Integer value) {
-    this.requestId = value;
-}
-@Id
-@GeneratedValue()
-public Integer getRequestId() {
-    return this.requestId;
-}
-private Student student;
+	@ManyToOne(optional = false)
+	public Tutor getTutor() {
+		return this.tutor;
+	}
 
-@ManyToOne(optional=false)
-public Student getStudent() {
-   return this.student;
-}
+	public void setTutor(Tutor tutor) {
+		this.tutor = tutor;
+	}
 
-public void setStudent(Student student) {
-   this.student = student;
-}
+	private Integer requestId;
 
-private Session session;
+	public void setRequestId(Integer value) {
+		this.requestId = value;
+	}
 
-@OneToOne(mappedBy="request" , cascade={CascadeType.ALL})
-public Session getSession() {
-   return this.session;
-}
+	@Id
+	@GeneratedValue()
+	public Integer getRequestId() {
+		return this.requestId;
+	}
 
-public void setSession(Session session) {
-   this.session = session;
-}
+	private Student student;
 
-private Time time;
+	@ManyToOne(optional = false)
+	public Student getStudent() {
+		return this.student;
+	}
 
-public void setTime(Time value) {
-    this.time = value;
-}
-public Time getTime() {
-    return this.time;
-}
-private Date date;
+	public void setStudent(Student student) {
+		this.student = student;
+	}
 
-public void setDate(Date value) {
-    this.date = value;
+	private Time time;
+
+	public void setTime(Time value) {
+		this.time = value;
+	}
+
+	public Time getTime() {
+		return this.time;
+	}
+
+	private Date date;
+
+	public void setDate(Date value) {
+		this.date = value;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	private Notification notification;
+
+	@OneToOne(mappedBy = "request", cascade = { CascadeType.ALL }, optional = false)
+	public Notification getNotification() {
+		return this.notification;
+	}
+
+	public void setNotification(Notification notification) {
+		this.notification = notification;
+	}
+
+	private Course course;
+
+	@ManyToOne(optional = false)
+	public Course getCourse() {
+		return this.course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+	private Room room;
+
+	@ManyToOne
+	public Room getRoom() {
+		return this.room;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
 }
-public Date getDate() {
-    return this.date;
-}
-   private Notification notification;
-   
-   @OneToOne(mappedBy="request" , cascade={CascadeType.ALL}, optional=false)
-   public Notification getNotification() {
-      return this.notification;
-   }
-   
-   public void setNotification(Notification notification) {
-      this.notification = notification;
-   }
-   
-   private Course course;
-   
-   @ManyToOne(optional=false)
-   public Course getCourse() {
-      return this.course;
-   }
-   
-   public void setCourse(Course course) {
-      this.course = course;
-   }
-   
-   }
--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/TimeSlot.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/model/TimeSlot.java
@@ -8,41 +8,47 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 
 @Entity
-public class TimeSlot{
-   private Tutor tutor;
-   
-   @ManyToOne(optional=false)
-   public Tutor getTutor() {
-      return this.tutor;
-   }
-   
-   public void setTutor(Tutor tutor) {
-      this.tutor = tutor;
-   }
-   
-   private Date date;
+public class TimeSlot {
+	private Tutor tutor;
 
-public void setDate(Date value) {
-    this.date = value;
-}
-public Date getDate() {
-    return this.date;
-}
-private Time time;
+	@ManyToOne(optional = false)
+	public Tutor getTutor() {
+		return this.tutor;
+	}
 
-public void setTime(Time value) {
-    this.time = value;
-}
-public Time getTime() {
-    return this.time;
-}
-private Integer timeSlotId;
+	public void setTutor(Tutor tutor) {
+		this.tutor = tutor;
+	}
 
-public void setTimeSlotId(Integer value) {
-    this.timeSlotId = value;
-}
-@Id
-@GeneratedValue()public Integer getTimeSlotId() {
-    return this.timeSlotId;
-}
+	private Date date;
+
+	public void setDate(Date value) {
+		this.date = value;
+	}
+
+	public Date getDate() {
+		return this.date;
+	}
+
+	private Time time;
+
+	public void setTime(Time value) {
+		this.time = value;
+	}
+
+	public Time getTime() {
+		return this.time;
+	}
+
+	private Integer timeSlotId;
+
+	public void setTimeSlotId(Integer value) {
+		this.timeSlotId = value;
+	}
+
+	@Id
+	@GeneratedValue()
+	public Integer getTimeSlotId() {
+		return this.timeSlotId;
+	}
 }
