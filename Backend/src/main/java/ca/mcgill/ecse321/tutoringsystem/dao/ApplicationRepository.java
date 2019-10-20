1d5bac1d60001c0abb991599744e701a469c48dd
Nelson Zeng <nelson.zeng25@gmail.com>
2019-10-20 17:26:28 -0400

    Added @RepositoryRestResource to all repository classes

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dao/ApplicationRepository.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dao/ApplicationRepository.java
@@ -1,9 +1,10 @@
 package ca.mcgill.ecse321.tutoringsystem.dao;
 
 import org.springframework.data.repository.CrudRepository;
-
 import ca.mcgill.ecse321.tutoringsystem.model.Application;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(collectionResourceRel = "applications", path = "applications")
 public interface ApplicationRepository extends CrudRepository<Application, Integer> {
 	Application findApplicationByApplicationId(int applicationId);
 
