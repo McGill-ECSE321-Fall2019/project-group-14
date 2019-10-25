00dfbe70e831db11057488e4128b2437a045ed18
Tian Ci Liu <the.liutianci@gmail.com>
2019-10-25 18:06:55 -0400

    added Dto converters

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/DtoConverter.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/DtoConverter.java
@@ -1,12 +1,16 @@
 package ca.mcgill.ecse321.tutoringsystem.controller;
 
-import ca.mcgill.ecse321.tutoringsystem.dto.ApplicationDto;
-import ca.mcgill.ecse321.tutoringsystem.dto.ManagerDto;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import ca.mcgill.ecse321.tutoringsystem.dto.*;
 import ca.mcgill.ecse321.tutoringsystem.model.*;
 
 public class DtoConverter {
 
-	private static ApplicationDto convertToDto(Application a) {
+	public static ApplicationDto toDto(Application a) {
 		if (a == null) {
 			throw new IllegalArgumentException("There is no such Application!");
 		}
@@ -14,7 +18,34 @@ public class DtoConverter {
 		return applicationDto;
 	}
 	
-	public static ManagerDto convertToDto(Manager m) {
+	public static CourseDto toDto(Course c) {
+		if (c == null) {
+			throw new IllegalArgumentException("There is no such Course!");
+		}
+		CourseDto courseDto = new CourseDto(c.getCourseName(), toDto(c.getInstitution()), c.getSubjectName(), toDto(c.getWage())); 
+		return courseDto;
+	}
+	
+	public static Set<CourseDto> toDto(Set<Course> c) {
+		if (c == null) {
+			throw new IllegalArgumentException("There is no such Course!");
+		}
+		Set<CourseDto> courses = new HashSet<>();
+		for (Course course : c) {
+			courses.add(toDto(course));
+		}
+		return courses;
+	}
+	
+	public static InstitutionDto toDto(Institution i) {
+		if (i == null) {
+			throw new IllegalArgumentException("There is no such Institution!");
+		}
+		InstitutionDto institutionDto = new InstitutionDto(i.getInstitutionName(), toDto(i.getCourses()), toDto(i.getInstitutionLevel()));
+		return institutionDto;
+	}
+	
+	public static ManagerDto toDto(Manager m) {
 		if (m == null) {
 			throw new IllegalArgumentException("There is no such Manager!");
 		}
