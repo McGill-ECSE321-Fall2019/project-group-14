d4a1985b2fb7ad9131d5ee41936856e3a5b4cc68
Steven <luu.stevenluu@gmail.com>
2019-10-25 19:51:16 -0400

    Time/Tutor/Wage Controller

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/TimeSlotController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/TimeSlotController.java
@@ -2,16 +2,18 @@ package ca.mcgill.ecse321.tutoringsystem.controller;
 
 import java.sql.Date;
 import java.sql.Time;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ca.mcgill.ecse321.tutoringsystem.dto.TimeSlotDto;
+import ca.mcgill.ecse321.tutoringsystem.dto.WageDto;
 import ca.mcgill.ecse321.tutoringsystem.model.TimeSlot;
 import ca.mcgill.ecse321.tutoringsystem.model.Tutor;
+import ca.mcgill.ecse321.tutoringsystem.model.Wage;
 import ca.mcgill.ecse321.tutoringsystem.service.TimeSlotService;
 
 @CrossOrigin(origins = "*")
@@ -21,17 +23,24 @@ public class TimeSlotController {
   @Autowired
   TimeSlotService timeSlotService;
 
-  @PostMapping(value = {"/timeslot", "/timeslot"})
-  public TimeSlotDto createTimeSlot(@PathVariable("id") Tutor id, @PathVariable("Date") Date date, @PathVariable("Time") Time time) throws IllegalArgumentException {
-      TimeSlot timeSlot = timeSlotService.createTimeSlot(id, date, time);
-      return convertToDto(timeSlot);
+  @PostMapping(value = {"/timeslot/create", "/timeslot/create"})
+  public TimeSlotDto createTimeSlot(@PathVariable("Id") Tutor id, @PathVariable("Date") Date date,
+      @PathVariable("Time") Time time) throws IllegalArgumentException {
+    TimeSlot timeSlot = timeSlotService.createTimeSlot(id, date, time);
+    return DtoConverter.toDto(timeSlot);
   }
-  
-  private TimeSlotDto convertToDto(TimeSlot timeSlot) {
-    if (timeSlot == null) {
-      throw new IllegalArgumentException("There is no such TimeSlot!");
-    }
-    TimeSlotDto timeSlotDto = new TimeSlotDto(timeSlot.getDate(),timeSlot.getTime(),timeSlot.getTimeSlotId());
-    return timeSlotDto;
+
+  @PostMapping(value = {"/timeslot", "/timeslot/"})
+  public TimeSlotDto getTimeSlotById(@PathVariable("id") Integer id) throws IllegalArgumentException {
+    TimeSlot timeSlot = timeSlotService.getTimeSlot(id);
+    return DtoConverter.toDto(timeSlot);
+  }
+
+  @PostMapping(value = {"/timeslot", "/timeslot/"})
+  public TimeSlotDto getTimeSlotByDateAndTime(@PathVariable("Date") Date date, @PathVariable("Time") Time time)
+      throws IllegalArgumentException {
+    TimeSlot timeSlot = timeSlotService.getTimeSlot(date, time);
+    return DtoConverter.toDto(timeSlot);
+
   }
 }
--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/WageController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/WageController.java
@@ -1,10 +1,40 @@
 package ca.mcgill.ecse321.tutoringsystem.controller;
 
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ca.mcgill.ecse321.tutoringsystem.dto.WageDto;
+import ca.mcgill.ecse321.tutoringsystem.model.Course;
+import ca.mcgill.ecse321.tutoringsystem.model.Tutor;
+import ca.mcgill.ecse321.tutoringsystem.model.Wage;
+import ca.mcgill.ecse321.tutoringsystem.service.WageService;
+
 
 @CrossOrigin(origins = "*")
 @RestController
 public class WageController {
+  @Autowired
+  WageService wageService;
 
+  @PostMapping(value = {"/wage/create", "/wage/create"})
+  public WageDto createTimSlot(@PathVariable("Id") Tutor tutor, @PathVariable("Date") Course course, @PathVariable("Time") Integer nb) throws IllegalArgumentException {
+      Wage wage = wageService.createWage(tutor, course, nb);
+      return DtoConverter.toDto(wage);
+  }
+  
+  @PostMapping(value = {"/wage", "/wage/"})
+  public WageDto getWageById(@PathVariable("id") Integer id) throws IllegalArgumentException {
+      Wage wage = wageService.getWage(id);
+      return DtoConverter.toDto(wage);
+  }
+  
+  @PostMapping(value = {"/allwages", "/allwages/"})
+  public Set<WageDto> getAllWages() throws IllegalArgumentException {
+      Set<Wage> wageSet= new HashSet<Wage>(wageService.getAllWages());
+      return DtoConverter.wageSetToDto(wageSet);
+  }
 }
