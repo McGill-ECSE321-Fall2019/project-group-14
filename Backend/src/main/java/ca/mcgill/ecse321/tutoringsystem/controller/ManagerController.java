32a75c263baff885ad07d2000f4a3c861ef9051f
mehdiammar <kabylfx@gmail.com>
2019-10-25 20:15:52 -0400

    manager, request, notification controller classes

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/ManagerController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/ManagerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.tutoringsystem.dto.*;
@@ -18,10 +19,9 @@ public class ManagerController {
 	@Autowired
 	ManagerService managerService;
 	
-	@PostMapping(value = { "/manager", "/manager/" })
-	public ManagerDto createPerson(@PathVariable("name") String name, @PathVariable("email") String email,
+	@PostMapping(value = { "/manager/create", "/manager/create/" })
+	public ManagerDto createPerson(@RequestParam("name") String name, @PathVariable("email") String email,
 			@PathVariable("password") String password) throws IllegalArgumentException {
-		// @formatter:on
 		Manager manager = managerService.createManager(name, email, password);
 		return DtoConverter.toDto(manager);
 	}
@@ -31,7 +31,7 @@ public class ManagerController {
 		return DtoConverter.toDto(managerService.getManager(id));
 	}
 
-	@GetMapping(value = { "/manager/{name}" })
+	@GetMapping(value = { "/manager/{email}" })
 	public ManagerDto getManagerById(@PathVariable("email") String email) throws IllegalArgumentException {
 		return DtoConverter.toDto(managerService.getManager(email));
 	}
