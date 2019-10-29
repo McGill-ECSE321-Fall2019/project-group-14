4f10727d838a4b737518c6296412cc7c7e831dc0
luusteve <45112541+luusteve@users.noreply.github.com>
2019-10-29 19:53:38 -0400

    Update WageController.java

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/WageController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/WageController.java
@@ -11,51 +11,54 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ca.mcgill.ecse321.tutoringsystem.dto.WageDto;
-import ca.mcgill.ecse321.tutoringsystem.model.Course;
-import ca.mcgill.ecse321.tutoringsystem.model.Tutor;
 import ca.mcgill.ecse321.tutoringsystem.model.Wage;
+import ca.mcgill.ecse321.tutoringsystem.service.CourseService;
+import ca.mcgill.ecse321.tutoringsystem.service.TutorService;
 import ca.mcgill.ecse321.tutoringsystem.service.WageService;
-
 
 @CrossOrigin(origins = "*")
 @RestController
 public class WageController {
   @Autowired
   WageService wageService;
+  @Autowired
+  TutorService tutorService;
+  @Autowired
+  CourseService courseService;
 
-  @PostMapping(value = {"/wage/create", "/wage/create/"})
-  public WageDto createTimSlot(@RequestParam("Id") Tutor tutor, @RequestParam("Course") Course course,
-      @RequestParam("Time") Integer nb) throws IllegalArgumentException {
-    Wage wage = wageService.createWage(tutor, course, nb);
+  @PostMapping(value = {"/wages/create", "/wages/create/"})
+  public WageDto createTimeSlot(@RequestParam("tutorId") Integer tutorId, @RequestParam("course") String courseName,
+      @PathVariable("wage") Integer wageAmount) throws IllegalArgumentException {
+    Wage wage = wageService.createWage(tutorService.getTutor(tutorId), courseService.getCourse(courseName), wageAmount);
     return DtoConverter.toDto(wage);
   }
 
-  @GetMapping(value = {"/wage/{id}", "/wage/{id}/"})
+  @GetMapping(value = {"/wages/{id}", "/wages/{id}/"})
   public WageDto getWageById(@PathVariable("id") Integer id) throws IllegalArgumentException {
     Wage wage = wageService.getWage(id);
     return DtoConverter.toDto(wage);
   }
+  
+  @DeleteMapping(value = {"/wages/{id}", "/wages/{id}/"})
+  public boolean deleteWageById(@PathVariable("id") Integer id) throws IllegalArgumentException {
+    return wageService.deleteWage(id);
+  }
 
-  @GetMapping(value = {"/allwages", "/allwages/"})
+  @GetMapping(value = {"/wages", "/wages/"})
   public Set<WageDto> getAllWages() throws IllegalArgumentException {
     Set<Wage> wageSet = new HashSet<Wage>(wageService.getAllWages());
     return DtoConverter.wageSetToDto(wageSet);
   }
 
-  @GetMapping(value = {"/allwagesbytutor", "/allwagesbytutor/"})
-  public Set<WageDto> getAllWagesByTutor(@RequestParam("Tutor") Tutor tutor) throws IllegalArgumentException {
-    Set<Wage> wageSet = new HashSet<Wage>(wageService.getTutorWages(tutor));
+  @GetMapping(value = {"/wages/tutor/{tutor}", "/wages/tutor/{tutor}/"})
+  public Set<WageDto> getAllWagesByTutor(@PathVariable("tutor") Integer tutorId) throws IllegalArgumentException {
+    Set<Wage> wageSet = new HashSet<Wage>(wageService.getTutorWages(tutorService.getTutor(tutorId)));
     return DtoConverter.wageSetToDto(wageSet);
   }
-  
-  @DeleteMapping(value = {"/timeslots/{id}", "/timeslots/{id}/"})
-  public boolean deleteWageById(@PathVariable("id") Integer id) throws IllegalArgumentException {
-    return wageService.deleteWage(id);
-  }
 
-  @GetMapping(value = {"/allwagesbycourse", "/allwagesbycourse/"})
-  public Set<WageDto> getAllWagesByCourse(@RequestParam("Course") Course course) throws IllegalArgumentException {
-    Set<Wage> wageSet = new HashSet<Wage>(wageService.getCourseWages(course));
+  @GetMapping(value = {"/wages/course/{course}", "/wages/course/{course}/"})
+  public Set<WageDto> getAllWagesByCourse(@PathVariable("course") String courseName) throws IllegalArgumentException {
+    Set<Wage> wageSet = new HashSet<Wage>(wageService.getCourseWages(courseService.getCourse(courseName)));
     return DtoConverter.wageSetToDto(wageSet);
   }
 
