252b3a18e2831ae2961d9cd7f1904703c5845f22
Mehdi <kabylfx@gmail.com>
2019-10-31 17:30:25 -0400

    testing rest services

--- Backend/src/test/java/ca/mcgill/ecse321/tutoringsystem/TutoringSystemIntegrationTests.java
+++ Backend/src/test/java/ca/mcgill/ecse321/tutoringsystem/TutoringSystemIntegrationTests.java
@@ -1,14 +1,88 @@
 package ca.mcgill.ecse321.tutoringsystem;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TutoringSystemIntegrationTests {
 
 	// Integration tests go here: Testing of REST
-	
+
+	private final String APP_URL = "https://tutoringsystem-backend-14.herokuapp.com";
+	private JSONObject response;
+	private final String restName = "Martin";
+	private final String restEmail = "emailTest";
+	private final String restPassword = "passwordTest";
+
 	@Test
 	public void contextLoads() {
 		System.out.println("ran Integration file");
+	}
+
+	@After
+	public void clearDatabase() {
+
+	}
+
+	@Test 
+    public void testCreateTutor() {
+        try {
+    		response = send("POST", APP_URL, "/tutors/create", "name=" + restName + "&email=" + restEmail + "&password=" + restPassword);
+            System.out.println("Received: " + response.toString());
+			assertEquals(restName, response.getString("name"));
+		} catch (JSONException e) {
+			fail();
+		}
+    }
+	
+	@Test 
+    public void testCreateRoom() {
+        try {
+    		response = send("POST", APP_URL, "/rooms/create", "id=21" + "&capacity=23");
+            System.out.println("Received: " + response.toString());
+			assertEquals(21, response.getString("capacity"));
+		} catch (JSONException e) {
+			fail();
+		}
+    }
+
+	public JSONObject send(String type, String appURL, String path, String parameters) {
+		try {
+			URL URL = new URL(appURL + path + ((parameters == null) ? "" : ("?" + parameters)));
+			System.out.println("Sending: " + URL.toString());
+			HttpURLConnection c = (HttpURLConnection) URL.openConnection();
+			c.setRequestMethod(type);
+			c.setRequestProperty("Accept", "application/json");
+			System.out.println(c.getContentType());
+			if (c.getResponseCode() != 200) {
+				throw new RuntimeException(URL.toString() + " Returned error: " + c.getResponseCode());
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader((c.getInputStream())));
+			String response = br.readLine();
+			if (response != null) {
+				JSONObject json = new JSONObject(response);
+				c.disconnect();
+				return json;
+			}
+		} catch (JSONException | IOException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
