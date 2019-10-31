d6fd4bd6e3acaa8386a8ec0ee345a7f1735f43d0
Nelson Zeng <nelson.zeng25@gmail.com>
2019-10-31 19:07:04 -0400

    Added comments to more complex methods

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/BusinessMethodController.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/controller/BusinessMethodController.java
@@ -21,46 +21,52 @@ import ca.mcgill.ecse321.tutoringsystem.service.RequestService;
 @CrossOrigin(origins = "*")
 @RestController
 public class BusinessMethodController {
-	@Autowired
-	PersonService personService;
-	@Autowired
-	RequestService requestService;
-	
-	@PostMapping(value = { "/login", "/login/" })
-	public PersonDto login(@RequestParam("email") String email, @RequestParam("password") String password) throws IllegalArgumentException {
-		Person person = personService.login(email, password);
-		if (person instanceof Manager) {
-			return DtoConverter.toDto(person); 
-		} else if (person instanceof Tutor) {
-			return DtoConverter.toDto(person); 
-		} else if (person instanceof Student) {
-			return DtoConverter.toDto(person); 
-		} else {
-			// login failed, returned person null
-			return null;
-		}
-	}
-	
-	@PostMapping(value = { "/accept/{id}", "/accept/{id}/" })
-	public RequestDto acceptRequest(@PathVariable("id") Integer id) throws IllegalArgumentException {
-		try {
+  @Autowired
+  PersonService personService;
+  @Autowired
+  RequestService requestService;
+
+  /**
+   * Logs in the user with input email and password
+   * 
+   * @param email
+   * @param password
+   * @return personDto or null if login failed
+   * @throws IllegalArgumentException
+   */
+  @PostMapping(value = {"/login", "/login/"})
+  public PersonDto login(@RequestParam("email") String email,
+      @RequestParam("password") String password) throws IllegalArgumentException {
+    Person person = personService.login(email, password);
+    if (person instanceof Manager) {
+      return DtoConverter.toDto(person);
+    } else if (person instanceof Tutor) {
+      return DtoConverter.toDto(person);
+    } else if (person instanceof Student) {
+      return DtoConverter.toDto(person);
+    } else {
+      return null;
+    }
+  }
+
+  @PostMapping(value = {"/accept/{id}", "/accept/{id}/"})
+  public RequestDto acceptRequest(@PathVariable("id") Integer id) throws IllegalArgumentException {
+    try {
       requestService.acceptRequest(id);
     } catch (IOException e) {
     }
-		Request request = requestService.getRequest(id);
-		if (request.getRoom() != null) {
-			return DtoConverter.toDto(request);
-		} else {
-			return null;
-		}
-	}
-	
-	@PostMapping(value = { "/reject/{id}", "/reject/{id}/" })
-	public void rejectRequest(@PathVariable("id") Integer id) throws IllegalArgumentException {
-		requestService.rejectRequest(id);
-		return;
-	}
-	
-	//TODO: reserve room for tutoring session (by manager)
-	//TODO: change account info
+    Request request = requestService.getRequest(id);
+    if (request.getRoom() != null) {
+      return DtoConverter.toDto(request);
+    } else {
+      return null;
+    }
+  }
+
+  @PostMapping(value = {"/reject/{id}", "/reject/{id}/"})
+  public void rejectRequest(@PathVariable("id") Integer id) throws IllegalArgumentException {
+    requestService.rejectRequest(id);
+    return;
+  }
+
 }
