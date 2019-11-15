91e9c7133cceb0022c2536e540431c0df3ec47a4
mehdiammar <kabylfx@gmail.com>
2019-11-14 20:23:39 -0500

    updated index.html, logo, vue files

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Apply from '@/components/Apply'
+import Review from '@/components/Review'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/apply',
+      name: 'Apply',
+      component: Apply
+    },
+    {
+      path: '/reviews',
+      name: 'Review',
+      component: Review
     }
   ]
 })
