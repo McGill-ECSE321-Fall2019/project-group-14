9ae6d801b2cb596fdf4dd9e51d79cc9f20c38bf4
Tian Ci Liu <the.liutianci@gmail.com>
2019-11-15 18:00:36 -0500

    modify WageDto so it actually returns the course name

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/WageDto.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/WageDto.java
@@ -1,36 +1,51 @@
 package ca.mcgill.ecse321.tutoringsystem.dto;
 
 public class WageDto {
-  private Integer wage;
-  private Integer wageId;
-  
-  public WageDto() {
-    
-  }
-  
-  public WageDto(Integer wageId) {
-    this(null, wageId);
-  }
-  
-  public WageDto(Integer wage, Integer wageId) {
-    this.wage = wage;
-    this.wageId = wageId;
-  }
+	private Integer wage;
+	private Integer wageId;
+	private String courseName;
 
-  public Integer getWage() {
-    return wage;
-  }
+	public WageDto() {
 
-  public void setWage(Integer wage) {
-    this.wage = wage;
-  }
+	}
 
-  public Integer getWageId() {
-    return wageId;
-  }
+	public WageDto(Integer wageId) {
+		this(null, wageId);
+	}
 
-  public void setWageId(Integer wageId) {
-    this.wageId = wageId;
-  }
-  
+	public WageDto(Integer wage, Integer wageId) {
+		this.wage = wage;
+		this.wageId = wageId;
+	}
+
+	public WageDto(Integer wage, Integer wageId, String courseName) {
+		this.wage = wage;
+		this.wageId = wageId;
+		this.courseName = courseName;
+	}
+
+	public Integer getWage() {
+		return wage;
+	}
+
+	public void setWage(Integer wage) {
+		this.wage = wage;
+	}
+
+	public Integer getWageId() {
+		return wageId;
+	}
+
+	public void setWageId(Integer wageId) {
+		this.wageId = wageId;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
 }
