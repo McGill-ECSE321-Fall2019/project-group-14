2419b82d858d0a41a803ad9daf69ec76f8ad80b0
mehdiammar <kabylfx@gmail.com>
2019-11-15 19:17:31 -0500

    welcome, reviews, create review, and apply frontend

--- Frontend/src/components/Review.vue
+++ Frontend/src/components/Review.vue
@@ -1,61 +1,49 @@
 <template>
-    <div class="col-lg-8 col-md-8" id = "component">
-    <h3 class="mb-30">Unreviewed sessions</h3>
-    <form action="#">
-      <div class="mt-10">
-        <input
-          type="text"
-          name="applicant_name"
-          placeholder="Please enter your full name"
-          onfocus="this.placeholder = ''"
-          onblur="this.placeholder = 'Please enter your full name'"
-          required
-          class="single-input"
-        />
+  <div class="col-lg-8 col-md-8" id="component">
+    <div class="col-md-4 mt-sm-30" id = "list">
+      <h3 class="mb-20">Unreviewed Sessions</h3>
+            <h6 class="mb-20">You have 24 hours to review a session with a student after it ended.</h6>
+      <div class>
+        <ul class="unordered-list">
+          <li>Fta Keys</li>
+          <li>For Women Only Your Computer Usage</li>
+          <li>Dealing With Technical Support 10 Useful Tips</li>
+          <li>Make Myspace Your Best Designed Space</li>
+          <li>Cleaning And Organizing Your Computer</li>
+        </ul>
       </div>
-      <div class="mt-10">
-        <input
-          type="email"
-          name="EMAIL"
-          placeholder="Email address"
-          onfocus="this.placeholder = ''"
-          onblur="this.placeholder = 'Email address'"
-          required
-          class="single-input"
-        />
-      </div>
-
-      <div class="mt-10">
-        <textarea
-          class="single-textarea"
-          placeholder="Please enter the list of courses you can teach"
-          onfocus="this.placeholder = ''"
-          onblur="this.placeholder = 'Please enter the list of courses you can teach'"
-          required
-        ></textarea>
-      </div>
-    </form>
-    <h3 class="mb-30">Your reviews</h3>
+    </div>
+    <h3 class="mb-30" id="reviews">Your reviews</h3>
     <a class="btn_1" href="#">Apply!</a>
   </div>
 </template>
 
-<script>
-export default {
-    
-}
+<script src="./review.js">
 </script>
 
 <style scoped>
- h3 {
+h3 {
   margin-top: 200px;
+  text-align: left;
+}
+h6 {
+  display: inline-block;
+  text-align: left;
+}
+#component {
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 10px;
+}
 
- }
+#reviews {
+  margin-top: 100px;
 
- #component {
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 10px;
+}
+ul {
+      margin-top: 50px;
+  display: inline-block;
+  text-align: left;
 }
 
 .btn_1 {
