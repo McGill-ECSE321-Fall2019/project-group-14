5a30e8a8a32ac9f2fa07e02df34cbd266eb85689
Tian Ci Liu <the.liutianci@gmail.com>
2019-11-15 19:16:01 -0500

    fix Dto circular calling attempt #3

--- Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/TutorDto.java
+++ Backend/src/main/java/ca/mcgill/ecse321/tutoringsystem/dto/TutorDto.java
@@ -6,7 +6,6 @@ import java.util.Set;
 public class TutorDto extends PersonDto {
   private Set<TimeSlotDto> timeSlots;
   private Set<WageDto> wages;
-  private Set<RequestDto> requests;
   private Set<NotificationDto> notifications;
   private Set<ReviewDto> reviews;
   
@@ -15,14 +14,13 @@ public class TutorDto extends PersonDto {
   
   @SuppressWarnings("unchecked")
   public TutorDto(String email) {
-      this(null, email, null, null, Collections.EMPTY_SET, Collections.EMPTY_SET, Collections.EMPTY_SET, Collections.EMPTY_SET, Collections.EMPTY_SET);
+      this(null, email, null, null, Collections.EMPTY_SET, Collections.EMPTY_SET, Collections.EMPTY_SET, Collections.EMPTY_SET);
   }
   
-  public TutorDto(String name, String email, Integer userId, String password, Set<TimeSlotDto> timeSlots, Set<WageDto> wages, Set<RequestDto> requests, Set<NotificationDto> notifications, Set<ReviewDto> reviews) {
+  public TutorDto(String name, String email, Integer userId, String password, Set<TimeSlotDto> timeSlots, Set<WageDto> wages, Set<NotificationDto> notifications, Set<ReviewDto> reviews) {
     super(name, email, userId, password);
     this.timeSlots = timeSlots;
     this.wages = wages;
-    this.requests = requests;
     this.notifications = notifications;
     this.reviews = reviews;
   }
@@ -41,14 +39,6 @@ public class TutorDto extends PersonDto {
 
   public void setWages(Set<WageDto> wages) {
     this.wages = wages;
-  }
-
-  public Set<RequestDto> getRequests() {
-    return requests;
-  }
-
-  public void setRequests(Set<RequestDto> requests) {
-    this.requests = requests;
   }
 
   public Set<NotificationDto> getNotifications() {
