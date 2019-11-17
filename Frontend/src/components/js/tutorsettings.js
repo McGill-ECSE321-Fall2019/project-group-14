c74d513031a33d52c2acab0cfa9a7e737fc3d253
Tian Ci Liu <the.liutianci@gmail.com>
2019-11-17 15:05:10 -0500

    fix tutorsettings.js

--- Frontend/src/components/js/tutorsettings.js
+++ Frontend/src/components/js/tutorsettings.js
@@ -27,55 +27,64 @@ export default {
         }
     },
     mounted() {
-        AXIOS.get('/tutors/' + this.$cookie.get('userId'))
-        .then(response => {
-            this.response = response.data
-            this.errorMsg =''
-            if (this.response != '') {
-                this.tutorName = this.response['name']
-                this.password = this.response['password']
-            }
-            else {
-                console.log('Errored')
-            }
-        })
-        .catch(e => {
-            console.log(e)
-        })
+        this.getTutorNamePass()
 
-        AXIOS.get('/wages/tutor/' + this.$cookie.get('userId'))
-        .then(response => {
-            this.response = response.data
-            this.errorMsg =''
-            if (this.response != '') {
-                this.wages = this.response
-            }
-            else {
-                console.log('Errored')
-            }
-        })
-        .catch(e => {
-            console.log(e)
-        })
+        this.getWages()
 
-        AXIOS.get('/timeslots/tutor/' + this.$cookie.get('userId'))
-        .then(response => {
-            this.response = response.data
-            this.errorMsg =''
-            if (this.response != '') {
-                this.timeslots = this.response
-                this.timeslots.sort((a, b) => ((a.date + a.time) > (b.date + b.time)) ? 1 : -1)
-            }
-            else {
-                console.log('Errored')
-            }
-        })
-        .catch(e => {
-            console.log(e)
-        })
+        this.getTimeslots()
     },
     methods: {
-        updatetutor (tutorName, password) {
+        getTutorNamePass() {
+            AXIOS.get('/tutors/' + this.$cookie.get('userId'))
+                .then(response => {
+                    this.response = response.data
+                    this.errorMsg = ''
+                    if (this.response != '') {
+                        this.tutorName = this.response['name']
+                        this.password = this.response['password']
+                    }
+                    else {
+                        console.log('Errored')
+                    }
+                })
+                .catch(e => {
+                    console.log(e)
+                })
+        },
+        getWages() {
+            AXIOS.get('/wages/tutor/' + this.$cookie.get('userId'))
+            .then(response => {
+                this.response = response.data
+                this.errorMsg = ''
+                if (this.response != '') {
+                    this.wages = this.response
+                }
+                else {
+                    console.log('Errored')
+                }
+            })
+            .catch(e => {
+                console.log(e)
+            })
+        },
+        getTimeslots() {
+            AXIOS.get('/timeslots/tutor/' + this.$cookie.get('userId'))
+            .then(response => {
+                this.response = response.data
+                this.errorMsg = ''
+                if (this.response != '') {
+                    this.timeslots = this.response
+                    this.timeslots.sort((a, b) => ((a.date + a.time) > (b.date + b.time)) ? 1 : -1)
+                }
+                else {
+                    console.log('Errored')
+                }
+            })
+            .catch(e => {
+                console.log(e)
+            })
+        },
+        updatetutor(tutorName, password) {
             console.log('updating with ' + tutorName + password)
             if (tutorName == '') {
                 var errorMessage = "Name cannot be empty"
@@ -89,81 +98,81 @@ export default {
                 this.errorMsg = errorMessage
                 return
             }
-            AXIOS.put('/tutors/update/' + this.$cookie.get('userId'), $.param({name: tutorName, password: password}))
-            .then(response => {
-                this.response = response.data
-                this.errorMsg =''
-                if (this.response == true) {
-                    this.$cookie.set('name', this.tutorName, { expires: '1h'})
-                    window.location.reload()
-                }
-                else {
-                    this.errorMsg = 'Error updating info!'
-                    console.log(this.errorMsg)
-                }
-            })
-            .catch(e => {
-                var errorMessage = e.response
-                console.log(e)
-                this.errorMsg = errorMessage
-            })
+            AXIOS.put('/tutors/update/' + this.$cookie.get('userId'), $.param({ name: tutorName, password: password }))
+                .then(response => {
+                    this.response = response.data
+                    this.errorMsg = ''
+                    if (this.response == true) {
+                        this.$cookie.set('name', this.tutorName, { expires: '1h' })
+                        window.location.reload()
+                    }
+                    else {
+                        this.errorMsg = 'Error updating info!'
+                        console.log(this.errorMsg)
+                    }
+                })
+                .catch(e => {
+                    var errorMessage = e.response
+                    console.log(e)
+                    this.errorMsg = errorMessage
+                })
         },
-        updateWage (updateWageId) {
-            AXIOS.put('/wages/update/' + updateWageId, $.param({wage: this.wageUpdateAmounts[updateWageId] * 100}))
-            .then(response => {
-                this.response = response.data
-                this.errorMsg =''
-                if (this.response == true) {
-                    window.location.reload()
-                }
-                else {
-                    this.errorMsg = 'Error updating info!'
-                    console.log(this.errorMsg)
-                }
-            })
-            .catch(e => {
-                var errorMessage = e.response
-                console.log(e)
-                this.errorMsg = errorMessage
-            })
+        updateWage(updateWageId) {
+            AXIOS.put('/wages/update/' + updateWageId, $.param({ wage: this.wageUpdateAmounts[updateWageId] * 100 }))
+                .then(response => {
+                    this.response = response.data
+                    this.errorMsg = ''
+                    if (this.response == true) {
+                        window.location.reload()
+                    }
+                    else {
+                        this.errorMsg = 'Error updating info!'
+                        console.log(this.errorMsg)
+                    }
+                })
+                .catch(e => {
+                    var errorMessage = e.response
+                    console.log(e)
+                    this.errorMsg = errorMessage
+                })
         },
-        addTimeslot (newDate, newTime) {
-            AXIOS.post('/timeslots/create', $.param({id: this.$cookie.get('userId'), date: newDate, time: newTime}))
-            .then(response => {
-                this.response = response.data
-                this.errorMsg =''
-                if (this.response != '') {
-                    window.location.reload()
-                }
-                else {
-                    this.errorMsg = 'Error creating Timeslot!'
-                    console.log(this.errorMsg)
-                }
-            })
-            .catch(e => {
-                var errorMessage = e.response
-                console.log(e)
-                this.errorMsg = errorMessage
-            })
+        addTimeslot(newDate, newTime) {
+            AXIOS.post('/timeslots/create', $.param({ id: this.$cookie.get('userId'), date: newDate, time: newTime }))
+                .then(response => {
+                    this.response = response.data
+                    this.errorMsg = ''
+                    if (this.response != '') {
+                        window.location.reload()
+                    }
+                    else {
+                        this.errorMsg = 'Error creating Timeslot!'
+                        console.log(this.errorMsg)
+                    }
+                })
+                .catch(e => {
+                    var errorMessage = e.response
+                    console.log(e)
+                    this.errorMsg = errorMessage
+                })
         },
-        deleteTimeslot (deleteTimeslotId) {
+        deleteTimeslot(deleteTimeslotId) {
             AXIOS.delete('/timeslots/' + deleteTimeslotId)
-            .then(response => {
-                this.response = response.data
-                this.errorMsg =''
-                if (this.response == true) {
-                    window.location.reload()
-                }
-                else {
-                    this.errorMsg = 'Error updating info!'
-                    console.log(this.errorMsg)
-                }
-            })
-            .catch(e => {
-                var errorMessage = e.response
-                console.log(e)
-                this.errorMsg = errorMessage
-            })
+                .then(response => {
+                    this.response = response.data
+                    this.errorMsg = ''
+                    if (this.response == true) {
+                        window.location.reload()
+                    }
+                    else {
+                        this.errorMsg = 'Error updating info!'
+                        console.log(this.errorMsg)
+                    }
+                })
+                .catch(e => {
+                    var errorMessage = e.response
+                    console.log(e)
+                    this.errorMsg = errorMessage
+                })
         }
     }
 }
