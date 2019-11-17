4073abd81356b7ea551765cbd8bf7ce2591553f1
mehdiammar <kabylfx@gmail.com>
2019-11-16 23:12:13 -0500

    view reviews, sessions

--- Frontend/src/components/js/review.js
+++ Frontend/src/components/js/review.js
@@ -1,48 +1,78 @@
 import axios from 'axios'
+import JQuery from 'jquery'
+let $ = JQuery
 var config = require('../../../config')
-
 var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var backendUrl = config.dev.backendHost + ':' + config.dev.backendPort
+var tutorId = ''
 
 var AXIOS = axios.create({
-    baseURL: backendUrl,
-    headers: { 'Access-Control-Allow-Origin': frontendUrl }
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
 })
 
-function ReviewDto(rating, comment, from, to) {
-    this.rating = rating
-    this.comment = comment
-    this.from = from
-    this.to = to
-}
-
-
 export default {
-    name: 'review',
-    data () {
-      return {
-        tutorReviews: [],
-      }
-    },
-  
-    methods: {
-      getReviews: function() { 
-        AXIOS.get('/reviews/' + userRating + '&' + comment + '&' + from + '&' + to, {}, {})
-        .then(response => {
-          this.tutorReviews.push(response.data)
-        })
-        .catch(e => {
-          this.errorReview = e;
-        });
-    },
-      getReviewsFromTutor: function(id) {
-        XIOS.get('/reviews/tutor/' + id, {}, {})
-        .then(response => {
-          this.tutorReviews.push(response.data)
-        })
-        .catch(e => {
-          this.errorReview = e;
-        });
-      }
+  name: 'review',
+  props: ['id'],
+  mounted() {
+    tutorId = this.getCookie('userId')
+    this.getReviewsFromTutor(tutorId)
+    this.getRequestsFromTutor(tutorId)
+
+  },
+  data() {
+    return {
+      tutorReviews: [],
+      acceptedRequests: [],
+      response: [],
+      cookieName: '',
+      errorReview: '',
+      studentId: ''
     }
+  },
+
+  methods: {
+    hello: function () {
+      // alert(tutorId); -> this is a test function
+    },
+    getReviewsFromTutor: function (id) {
+      AXIOS.get('/reviews/tutor/' + id)
+        .then(response => {
+          this.tutorReviews = response.data
+        })
+        .catch(e => {
+          this.errorReview = e;
+          console.log(e)
+        })
+        ;
+    },
+    getRequestsFromTutor: function (id) {
+      AXIOS.get('/requests/accepted/' + id)
+        .then(response => {
+          this.acceptedRequests = response.data
+        })
+        .catch(e => {
+          this.errorReview = e;
+          console.log(e)
+        })
+        ;
+    },
+
+    getCookie: function (cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    },
+
+  }
 }
